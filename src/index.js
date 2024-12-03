121edd48288fec3555232c542b75ba151a14f41c
aeccl-mcneilus <andy@concise-ways.com>
2024-12-03 10:28:14 -0600

    debug

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
     console.log("hi!!!!")
     const j = await( await fetch(`/api/httpTrigger1`));
     // document.querySelector('#name').textContent = text;
-    console.log("json", j, typeof j)
     try {
-        const out = j.json()
+        console.log("json!!!!!", j, typeof j)
+        const out = await j.json()
         console.log("try out!!!", out)
     } catch (error) {
         console.log("ERRR", error)
