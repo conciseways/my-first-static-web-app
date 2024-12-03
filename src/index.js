c7b427f87f82762178f3fb9370083139dbb8d660
aeccl-mcneilus <andy@concise-ways.com>
2024-12-03 11:02:23 -0600

    awati added

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
     const j = await( await fetch(`/api/httpTrigger1`));
     // document.querySelector('#name').textContent = text;
     try {
-        console.log("try out!!!", j.text(), typeof j)
-        console.log("try out body!!!", j.body.text(), typeof j)
+        console.log("try out!!!",await j.text(), typeof j)
+        console.log("try out body!!!", await j.body.text(), typeof j)
         const out = await j.json()
         console.log("try out!!!", out)
     } catch (error) {
