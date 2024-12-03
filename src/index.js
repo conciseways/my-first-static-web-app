f6bbc11d2d8c2cc59045c56563821bcdadc8b949
aeccl-mcneilus <andy@concise-ways.com>
2024-12-03 10:23:21 -0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-(function iffe() {
+(async function iffe() {
     console.log("from static file js")
+    console.log("hi!!!!")
+    const j = await( await fetch(`/api/httpTrigger1`));
+    // document.querySelector('#name').textContent = text;
+    console.log("json", j, typeof j)
+    try {
+        const out = j.json()
+        console.log("try out!!!", out)
+    } catch (error) {
+        console.log("ERRR", error)
+    }
 })()
 
