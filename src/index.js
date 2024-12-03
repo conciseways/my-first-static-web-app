801400ddaf9d9b920af017ddb18813b743590fe3
aeccl-mcneilus <andy@concise-ways.com>
2024-12-03 10:55:21 -0600

    get reader

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@
     const j = await( await fetch(`/api/httpTrigger1`));
     // document.querySelector('#name').textContent = text;
     try {
-        console.log("try out!!!", j, typeof j)
-        console.log("try out body!!!", j.body, typeof j)
+        console.log("try out!!!", j.getReader(), typeof j)
+        console.log("try out body!!!", j.body.getReader(), typeof j)
         const out = await j.json()
         console.log("try out!!!", out)
     } catch (error) {
