9eddc4075601a9d44033d56947e8fdfc3bd469ad
aeccl-mcneilus <andy@concise-ways.com>
2024-12-04 10:49:43 -0600

    added 2nd trigger

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 (async function iffe() {
-    console.log("from static file js")
-    console.log("hi!!!!")
-    const j = await( await fetch(`/api/httpTrigger1`));
-    // document.querySelector('#name').textContent = text;
-    try {
-        console.log("try out!!!",await j.text(), typeof j)
-        console.log("try out body!!!", await j.body.text(), typeof j)
-        const out = await j.json()
-        console.log("try out!!!", out)
-    } catch (error) {
-        console.log("ERRR", error)
-    }
+    const $nameEl = document.getElementById("name")
+    const stringMsg = await fetch(`/api/httpTrigger1`);
+    const outStr = await stringMsg.text()
+    $nameEl.append(outStr)
 })()
 
