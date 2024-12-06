6cc224a54dae0af0cf52f109efd51c55d671a8e4
aeccl-mcneilus <andy@concise-ways.com>
2024-12-06 12:02:44 -0600

    trying to use axios

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import axios from './lib/axios'
 (async function iffe() {
     
-
     const $nameEl = document.getElementById("name")
     const stringMsg = await fetch(`/api/httpTrigger1`);
     const outStr = await stringMsg.text()
     $nameEl.append(outStr)
+
+    axios.get('httpTrigger2')
+    .then(res => console.log("httpTrigger2 res", res))
 })()
 
