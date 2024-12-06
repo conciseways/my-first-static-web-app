8becaf5f82f37b074b881853d775c2a8080f517d
aeccl-mcneilus <andy@concise-ways.com>
2024-12-06 09:26:10 -0600

    added axios to client

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 (async function iffe() {
+    
+
     const $nameEl = document.getElementById("name")
     const stringMsg = await fetch(`/api/httpTrigger1`);
     const outStr = await stringMsg.text()
